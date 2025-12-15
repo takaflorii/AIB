8128245abb865fefb5846d1b286699509fdf14f0
takaflorii <ngcsoares17@gmail.com>
2025-12-15 09:41:20 +0000

    Tarefa Extra SomaWhile

---
+++ Python12/Python12/Python12/SomaWhile.py
@@ -0,0 +1,8 @@
+num= int(input("Digite um número inteiro (0 para terminar)"))
+soma = 0
+while num!=0:
+    soma = soma + num
+    num= int(input("Digite um número inteiro (0 para terminar)"))
+    
+    
+print("A soma foi ", soma)
