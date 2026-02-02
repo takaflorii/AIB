16048ba6aa95ea245dc4d1d660ec1ae338e27d37
takaflorii <ngcsoares17@gmail.com>
2026-02-02 14:57:46 +0000

    Atualização projeto final

--- Python12/Projeto Final/Disciplinas.py
+++
@@ -1,9 +0,0 @@
-def adicionar_disciplina(nome):
-    nome = input("Digite o nome da disciplina: ")
-
-    disciplina = {
-        'nome': nome
-    }
-
-    dados['Disciplinas'].append(disciplina)
-    print(f"Disciplina '{nome}' adicionada com sucesso!")
