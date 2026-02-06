12cb38179b333545c9a57daf1685c3b3157b94d8
takaflorii <ngcsoares17@gmail.com>
2026-02-06 15:15:14 +0000

    NumpyArrays

---
+++ Python12/Funções/NumpyArrays.py
@@ -0,0 +1,11 @@
+import numpy as np
+""" Array bidimensional, 3 linhas e
+2 colunas , criado e inicializado a uns"""
+print( 'Array bidimensional inicializado a uns' )
+n_ones=np.ones(shape=(3,2),dtype= int )
+print(n_ones)
+"""Array unidimensional 4 elementos , criado e
+inicializado a uns"""
+print( 'Array unidimensional, criado e inicializado a uns ' )
+n_ones2=np.ones(shape=(4))
+print(n_ones2)
